293686aa4f40315cd13fb93dee4b6b90cdc6df35
Pedro Conti <pedrohmconti@gmail.com>
2026-09-22 09:03:25 -0300

    implementando paginação

--- src/main/java/com/algaworks/algasensors/device/management/api/controller/SensorController.java
+++ src/main/java/com/algaworks/algasensors/device/management/api/controller/SensorController.java
@@ -6,10 +6,14 @@ import com.algaworks.algasensors.device.management.common.IdGenerator;
 import com.algaworks.algasensors.device.management.domain.model.Sensor;
 import com.algaworks.algasensors.device.management.domain.model.SensorId;
 import com.algaworks.algasensors.device.management.domain.repository.SensorRepository;
-import lombok.AccessLevel;
+import io.hypersistence.tsid.TSID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/sensors")
@@ -17,6 +21,13 @@ import org.springframework.web.bind.annotation.*;
 public class SensorController {
 
     private final SensorRepository sensorRepository;
+
+    @GetMapping()
+    public Page<SensorOutput> search(@PageableDefault Pageable pageable){
+        Page<Sensor> page = sensorRepository.findAll(pageable);
+        return page.map(this::converteToModel);
+
+    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -31,6 +42,10 @@ public class SensorController {
                 .enabled(false)
                 .build();
         sensor =  sensorRepository.saveAndFlush(sensor);
+        return converteToModel(sensor);
+    }
+
+    private SensorOutput converteToModel(Sensor sensor) {
         return SensorOutput.builder()
                 .id(sensor.getId().getValue())
                 .name(sensor.getName())
@@ -41,5 +56,12 @@ public class SensorController {
                 .enabled(sensor.getEnabled())
                 .build();
     }
+
+    @GetMapping("{sensorId}")
+    public SensorOutput getOne(@PathVariable TSID sensorId){
+        Sensor sensor = sensorRepository.findById(new SensorId(sensorId)).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        return converteToModel(sensor);
+    }
+
 
 }
